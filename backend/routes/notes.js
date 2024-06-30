8f5430079e78694aa94dbc274d9c5e46abbf6542
Mashal <Mashal.maqsood112@gmail.com>
2024-06-30 13:30:00 -0700

    Implemented edit and delete endpoints for notes.

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -48,4 +48,53 @@ router.post(
     }
   }
 );
+
+router.put(
+  "/updateNote/:id",
+  fetchUser,
+  [
+    body("title", "enter a valid title").isLength({ min: 3 }),
+    body("description", "Description must be atleast 5 characters").isLength({
+      min: 5,
+    }),
+  ],
+  async (req, res) => {
+    try {
+      const { title, description, tag } = req.body;
+      const newNote = {};
+      if (title) newNote.title = title;
+      if (description) newNote.description = description;
+      if (tag) newNote.tag = tag;
+      let note = await Note.findById(req.params.id);
+      if (!note) return res.status(404).send("Not found");
+      if (note.user.toString() !== req.user.id) {
+        return res.status(401).send("Not Allowed");
+      }
+      note = await Note.findByIdAndUpdate(
+        req.params.id,
+        { $set: newNote },
+        { new: true }
+      );
+      res.json(note);
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).send("Internal server error");
+    }
+  }
+);
+
+router.delete("/deleteNote/:id", fetchUser, async (req, res) => {
+  try {
+    let note = await Note.findById(req.params.id);
+    if (!note) return res.status(404).send("Not found");
+    if (note.user.toString() !== req.user.id) {
+      return res.status(401).send("Not Allowed");
+    }
+    note = await Note.findByIdAndDelete(req.params.id);
+    res.json({ Success: "Note has been deleted", note });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send("Internal server error");
+  }
+});
 module.exports = router;
